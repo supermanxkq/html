92477cba56e296ac95e20510f1d5eb3d82473f62
李娜 <819580352@qq.com>
2018-09-03 11:21:26 +0800

    添加校验

--- populus/js/reserve.js
+++ populus/js/reserve.js
@@ -1,3 +1,25 @@
 $(function(){
-	$('#spinner').spinner()
+	$('#spinner').spinner();
+	
+	$('#tel').blur(function(){
+		if($(this).val()!==""){
+			if($(this).val().match(/^1[3|4|5|7|8|][0-9]{9}$/)){
+				$(this).next('span').text(" ");
+			}else{
+				$(this).next('span').text("*请输入正确的电话号码！");
+			}
+		}
+		
+	});
+	
+	$('#mail').blur(function(){
+		if($(this).val()!==""){
+			if($(this).val().match(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)){
+				$(this).next('span').text(" ");
+			}else{
+				$(this).next('span').text("*请输入正确的邮箱！");
+			}
+		}
+		
+	});
 });
